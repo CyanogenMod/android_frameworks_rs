f2748276ea84bed83728e38a6661946b5fb3bc5c
Jason Sams <jsams@google.com>
2013-11-26 18:10:59 -0800

    implement finish
    Change-Id: If1ec99d0dbcf7aebf9d90d7779bf3ff34a8963f2

--- rsContext.cpp
+++ rsContext.cpp
@@ -510,6 +510,7 @@ Context::Context() {
     mDPI = 96;
     mIsContextLite = false;
     memset(&watchdog, 0, sizeof(watchdog));
+    memset(&mHal, 0, sizeof(mHal));
     mForceCpu = false;
     mContextType = RS_CONTEXT_TYPE_NORMAL;
     mSynchronous = false;
@@ -721,6 +722,12 @@ void Context::setFont(Font *f) {
 }
 #endif
 
+void Context::finish() {
+    if (mHal.funcs.finish) {
+        mHal.funcs.finish(this);
+    }
+}
+
 void Context::assignName(ObjectBase *obj, const char *name, uint32_t len) {
     rsAssert(!obj->getName());
     obj->setName(name, len);
@@ -785,6 +792,7 @@ namespace android {
 namespace renderscript {
 
 void rsi_ContextFinish(Context *rsc) {
+    rsc->finish();
 }
 
 void rsi_ContextBindRootScript(Context *rsc, RsScript vs) {
