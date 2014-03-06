warpboard/platform_frameworks_rs

Add VP9 inter-frame prediction intrinsic
Change-Id: If8985a6200fb6d34083eff711ccdf2f1b3c374e6

cpp/rsCppStructs.h
@@ -1428,6 +1428,20 @@ class ScriptIntrinsic3DLUT : public ScriptIntrinsic {
      */
     void setLUT(sp<Allocation> lut);
 };
+/**
+ * Intrinsic for InterPrediction
+ */
+class ScriptIntrinsicInterPred : public ScriptIntrinsic {
+ private:
+    ScriptIntrinsicInterPred(sp<RS> rs, sp<const Element> e);
+ public:
+    static sp<ScriptIntrinsicInterPred> create(sp<RS> rs, sp<const Element> e);
+
+    void forEach(sp<Allocation> asize);
+    void setRef(sp<Allocation> ref);
+    void setParamCount(int fri, int sec, int offset);
+    void setParam(sp<Allocation> param);
+};
 
 /**
  * Intrinsic kernel for blending two Allocations.

CyanogenMod/android_frameworks_rs

cpp/rsCppStructs.h
@@ -1435,6 +1435,20 @@ class ScriptIntrinsic3DLUT : public ScriptIntrinsic {
      */
     void setLUT(sp<Allocation> lut);
 };
+/**
+ * Intrinsic for VP9InterPrediction
+ */
+class ScriptIntrinsicVP9InterPred : public ScriptIntrinsic {
+ private:
+    ScriptIntrinsicVP9InterPred(sp<RS> rs, sp<const Element> e);
+ public:
+    static sp<ScriptIntrinsicVP9InterPred> create(sp<RS> rs, sp<const Element> e);
+
+    void forEach(sp<Allocation> asize);
+    void setRef(sp<Allocation> ref);
+    void setParamCount(int fri, int sec, int offset);
+    void setParam(sp<Allocation> param);
+};
 
 /**
  * Intrinsic kernel for blending two Allocations.