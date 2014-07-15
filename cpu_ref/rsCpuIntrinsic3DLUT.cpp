Schischu/aosp_android_framework_rs

Split the RsForEachStubParamStruct in two.
This patch splits the RsForEachStubParamStruct into two smaller structs, one
used specifically by the driver and the other by the expanded kernels.  Doing
so makes it clearer what data is used where.  In addition, fewer data are
copied between memory locations during kernel invocation.

Several fields that were not being used were removed from the structs.

Change-Id: I7788ef754add44463b17a6b571c7cde6e73b9712

cpu_ref/rsCpuIntrinsic3DLUT.cpp
@@ -38,7 +38,7 @@ public:
 protected:
     ObjectBaseRef<Allocation> mLUT;
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsForEachKernelStruct *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
 };
@@ -58,7 +58,7 @@ extern "C" void rsdIntrinsic3DLUT_K(void *dst, void const *in, size_t count,
                                       int dimx, int dimy, int dimz);
 
 
-void RsdCpuScriptIntrinsic3DLUT::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsic3DLUT::kernel(const RsForEachKernelStruct *p,
                                       uint32_t xstart, uint32_t xend,
                                       uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsic3DLUT *cp = (RsdCpuScriptIntrinsic3DLUT *)p->usr;

cpu_ref/rsCpuIntrinsicBlend.cpp
@@ -33,7 +33,7 @@ public:
     RsdCpuScriptIntrinsicBlend(RsdCpuReferenceImpl *ctx, const Script *s, const Element *e);
 
 protected:
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsForEachKernelStruct *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
 };
@@ -110,7 +110,7 @@ extern "C" void rsdIntrinsicBlendAdd_K(void *dst, const void *src, uint32_t coun
 extern "C" void rsdIntrinsicBlendSub_K(void *dst, const void *src, uint32_t count8);
 #endif
 
-void RsdCpuScriptIntrinsicBlend::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicBlend::kernel(const RsForEachKernelStruct *p,
                                         uint32_t xstart, uint32_t xend,
                                         uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicBlend *cp = (RsdCpuScriptIntrinsicBlend *)p->usr;

cpu_ref/rsCpuIntrinsicColorMatrix.cpp
@@ -179,7 +179,7 @@ protected:
     FunctionTab_t mFnTab;
 #endif
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsForEachKernelStruct *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
     void updateCoeffCache(float fpMul, float addMul);
@@ -769,7 +769,7 @@ void RsdCpuScriptIntrinsicColorMatrix::setGlobalVar(uint32_t slot, const void *d
 }
 
 
-static void One(const RsForEachStubParamStruct *p, void *out,
+static void One(const RsForEachKernelStruct *p, void *out,
                 const void *py, const float* coeff, const float *add,
                 uint32_t vsin, uint32_t vsout, bool fin, bool fout) {
 
@@ -870,7 +870,7 @@ static void One(const RsForEachStubParamStruct *p, void *out,
     //ALOGE("out %p %f %f %f %f", out, ((float *)out)[0], ((float *)out)[1], ((float *)out)[2], ((float *)out)[3]);
 }
 
-void RsdCpuScriptIntrinsicColorMatrix::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicColorMatrix::kernel(const RsForEachKernelStruct *p,
                                               uint32_t xstart, uint32_t xend,
                                               uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicColorMatrix *cp = (RsdCpuScriptIntrinsicColorMatrix *)p->usr;

cpu_ref/rsCpuIntrinsicHistogram.cpp
@@ -49,29 +49,29 @@ protected:
     int *mSums;
     ObjectBaseRef<Allocation> mAllocOut;
 
-    static void kernelP1U4(const RsForEachStubParamStruct *p,
+    static void kernelP1U4(const RsForEachKernelStruct *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U3(const RsForEachStubParamStruct *p,
+    static void kernelP1U3(const RsForEachKernelStruct *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U2(const RsForEachStubParamStruct *p,
+    static void kernelP1U2(const RsForEachKernelStruct *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U1(const RsForEachStubParamStruct *p,
+    static void kernelP1U1(const RsForEachKernelStruct *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
 
-    static void kernelP1L4(const RsForEachStubParamStruct *p,
+    static void kernelP1L4(const RsForEachKernelStruct *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L3(const RsForEachStubParamStruct *p,
+    static void kernelP1L3(const RsForEachKernelStruct *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L2(const RsForEachStubParamStruct *p,
+    static void kernelP1L2(const RsForEachKernelStruct *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L1(const RsForEachStubParamStruct *p,
+    static void kernelP1L1(const RsForEachKernelStruct *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
 
@@ -160,7 +160,7 @@ void RsdCpuScriptIntrinsicHistogram::postLaunch(uint32_t slot, const Allocation 
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -177,7 +177,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -193,7 +193,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -208,7 +208,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -226,7 +226,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -243,7 +243,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -259,7 +259,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -274,7 +274,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U1(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U1(const RsForEachKernelStruct *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 

cpu_ref/rsCpuIntrinsicYuvToRGB.cpp
@@ -46,7 +46,7 @@ public:
 protected:
     ObjectBaseRef<Allocation> alloc;
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsForEachKernelStruct *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
 };
@@ -101,7 +101,7 @@ extern "C" void rsdIntrinsicYuv_K(void *dst, const uchar *Y, const uchar *uv, ui
 extern "C" void rsdIntrinsicYuvR_K(void *dst, const uchar *Y, const uchar *uv, uint32_t xstart, size_t xend);
 extern "C" void rsdIntrinsicYuv2_K(void *dst, const uchar *Y, const uchar *u, const uchar *v, size_t xstart, size_t xend);
 
-void RsdCpuScriptIntrinsicYuvToRGB::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicYuvToRGB::kernel(const RsForEachKernelStruct *p,
                                            uint32_t xstart, uint32_t xend,
                                            uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicYuvToRGB *cp = (RsdCpuScriptIntrinsicYuvToRGB *)p->usr;

cpu_ref/rsCpuScriptGroup.cpp
@@ -44,66 +44,71 @@ void CpuScriptGroupImpl::setOutput(const ScriptKernelID *kid, Allocation *a) {
 }
 
 
-typedef void (*ScriptGroupRootFunc_t)(const RsForEachStubParamStruct *p,
+typedef void (*ScriptGroupRootFunc_t)(const RsForEachKernelStruct *kparams,
                                       uint32_t xstart, uint32_t xend,
                                       uint32_t instep, uint32_t outstep);
 
-void CpuScriptGroupImpl::scriptGroupRoot(const RsForEachStubParamStruct *p,
+void CpuScriptGroupImpl::scriptGroupRoot(const RsForEachKernelStruct *kparams,
                                          uint32_t xstart, uint32_t xend,
                                          uint32_t instep, uint32_t outstep) {
 
 
-    const ScriptList *sl = (const ScriptList *)p->usr;
-    RsForEachStubParamStruct *mp = (RsForEachStubParamStruct *)p;
-    const void *oldUsr = p->usr;
+    const ScriptList      *sl       = (const ScriptList *)kparams->usr;
+    RsForEachKernelStruct *mkparams = (RsForEachKernelStruct *)kparams;
 
-    for(size_t ct=0; ct < sl->count; ct++) {
+    for (size_t ct = 0; ct < sl->count; ct++) {
         ScriptGroupRootFunc_t func;
-        func = (ScriptGroupRootFunc_t)sl->fnPtrs[ct];
-        mp->usr = sl->usrPtrs[ct];
+        func          = (ScriptGroupRootFunc_t)sl->fnPtrs[ct];
+        mkparams->usr = sl->usrPtrs[ct];
 
-        mp->ptrIn = NULL;
-        mp->in = NULL;
-        mp->ptrOut = NULL;
-        mp->out = NULL;
+        mkparams->in  = NULL;
+        mkparams->out = NULL;
 
         uint32_t istep = 0;
         uint32_t ostep = 0;
 
         if (sl->ins[ct]) {
-            mp->ptrIn = (const uint8_t *)sl->ins[ct]->mHal.drvState.lod[0].mallocPtr;
+            mkparams->in =
+              (const uint8_t *)sl->ins[ct]->mHal.drvState.lod[0].mallocPtr;
+
             istep = sl->ins[ct]->mHal.state.elementSizeBytes;
-            mp->in = mp->ptrIn;
+
             if (sl->inExts[ct]) {
-                mp->in = mp->ptrIn + sl->ins[ct]->mHal.drvState.lod[0].stride * p->y;
-            } else {
-                if (sl->ins[ct]->mHal.drvState.lod[0].dimY > p->lid) {
-                    mp->in = mp->ptrIn + sl->ins[ct]->mHal.drvState.lod[0].stride * p->lid;
-                }
+                mkparams->in =
+                  (const uint8_t *)mkparams->in +
+                  sl->ins[ct]->mHal.drvState.lod[0].stride * kparams->y;
+
+            } else if (sl->ins[ct]->mHal.drvState.lod[0].dimY > kparams->lid) {
+                mkparams->in =
+                  (const uint8_t *)mkparams->in +
+                  sl->ins[ct]->mHal.drvState.lod[0].stride * kparams->lid;
             }
         }
 
         if (sl->outs[ct]) {
-            mp->ptrOut = (uint8_t *)sl->outs[ct]->mHal.drvState.lod[0].mallocPtr;
-            mp->out = mp->ptrOut;
+            mkparams->out =
+              (uint8_t *)sl->outs[ct]->mHal.drvState.lod[0].mallocPtr;
+
             ostep = sl->outs[ct]->mHal.state.elementSizeBytes;
+
             if (sl->outExts[ct]) {
-                mp->out = mp->ptrOut + sl->outs[ct]->mHal.drvState.lod[0].stride * p->y;
-            } else {
-                if (sl->outs[ct]->mHal.drvState.lod[0].dimY > p->lid) {
-                    mp->out = mp->ptrOut + sl->outs[ct]->mHal.drvState.lod[0].stride * p->lid;
-                }
+                mkparams->out =
+                  (uint8_t *)mkparams->out +
+                  sl->outs[ct]->mHal.drvState.lod[0].stride * kparams->y;
+
+            } else if (sl->outs[ct]->mHal.drvState.lod[0].dimY > kparams->lid) {
+                mkparams->out =
+                  (uint8_t *)mkparams->out +
+                  sl->outs[ct]->mHal.drvState.lod[0].stride * kparams->lid;
             }
         }
 
         //ALOGE("kernel %i %p,%p  %p,%p", ct, mp->ptrIn, mp->in, mp->ptrOut, mp->out);
-        func(p, xstart, xend, istep, ostep);
+        func(kparams, xstart, xend, istep, ostep);
     }
     //ALOGE("script group root");
 
-    //ConvolveParams *cp = (ConvolveParams *)p->usr;
-
-    mp->usr = oldUsr;
+    mkparams->usr = sl;
 }
 
 

CyanogenMod/android_frameworks_rs

cpu_ref/rsCpuIntrinsic3DLUT.cpp
@@ -38,7 +38,7 @@ public:
 protected:
     ObjectBaseRef<Allocation> mLUT;
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsExpandKernelParams *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
 };
@@ -58,7 +58,7 @@ extern "C" void rsdIntrinsic3DLUT_K(void *dst, void const *in, size_t count,
                                       int dimx, int dimy, int dimz);
 
 
-void RsdCpuScriptIntrinsic3DLUT::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsic3DLUT::kernel(const RsExpandKernelParams *p,
                                       uint32_t xstart, uint32_t xend,
                                       uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsic3DLUT *cp = (RsdCpuScriptIntrinsic3DLUT *)p->usr;

cpu_ref/rsCpuIntrinsicBlend.cpp
@@ -33,7 +33,7 @@ public:
     RsdCpuScriptIntrinsicBlend(RsdCpuReferenceImpl *ctx, const Script *s, const Element *e);
 
 protected:
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsExpandKernelParams *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
 };
@@ -110,7 +110,7 @@ extern "C" void rsdIntrinsicBlendAdd_K(void *dst, const void *src, uint32_t coun
 extern "C" void rsdIntrinsicBlendSub_K(void *dst, const void *src, uint32_t count8);
 #endif
 
-void RsdCpuScriptIntrinsicBlend::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicBlend::kernel(const RsExpandKernelParams *p,
                                         uint32_t xstart, uint32_t xend,
                                         uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicBlend *cp = (RsdCpuScriptIntrinsicBlend *)p->usr;

cpu_ref/rsCpuIntrinsicColorMatrix.cpp
@@ -179,7 +179,7 @@ protected:
     FunctionTab_t mFnTab;
 #endif
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsExpandKernelParams *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
     void updateCoeffCache(float fpMul, float addMul);
@@ -769,7 +769,7 @@ void RsdCpuScriptIntrinsicColorMatrix::setGlobalVar(uint32_t slot, const void *d
 }
 
 
-static void One(const RsForEachStubParamStruct *p, void *out,
+static void One(const RsExpandKernelParams *p, void *out,
                 const void *py, const float* coeff, const float *add,
                 uint32_t vsin, uint32_t vsout, bool fin, bool fout) {
 
@@ -870,7 +870,7 @@ static void One(const RsForEachStubParamStruct *p, void *out,
     //ALOGE("out %p %f %f %f %f", out, ((float *)out)[0], ((float *)out)[1], ((float *)out)[2], ((float *)out)[3]);
 }
 
-void RsdCpuScriptIntrinsicColorMatrix::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicColorMatrix::kernel(const RsExpandKernelParams *p,
                                               uint32_t xstart, uint32_t xend,
                                               uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicColorMatrix *cp = (RsdCpuScriptIntrinsicColorMatrix *)p->usr;

cpu_ref/rsCpuIntrinsicHistogram.cpp
@@ -49,29 +49,29 @@ protected:
     int *mSums;
     ObjectBaseRef<Allocation> mAllocOut;
 
-    static void kernelP1U4(const RsForEachStubParamStruct *p,
+    static void kernelP1U4(const RsExpandKernelParams *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U3(const RsForEachStubParamStruct *p,
+    static void kernelP1U3(const RsExpandKernelParams *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U2(const RsForEachStubParamStruct *p,
+    static void kernelP1U2(const RsExpandKernelParams *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
-    static void kernelP1U1(const RsForEachStubParamStruct *p,
+    static void kernelP1U1(const RsExpandKernelParams *p,
                           uint32_t xstart, uint32_t xend,
                           uint32_t instep, uint32_t outstep);
 
-    static void kernelP1L4(const RsForEachStubParamStruct *p,
+    static void kernelP1L4(const RsExpandKernelParams *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L3(const RsForEachStubParamStruct *p,
+    static void kernelP1L3(const RsExpandKernelParams *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L2(const RsForEachStubParamStruct *p,
+    static void kernelP1L2(const RsExpandKernelParams *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
-    static void kernelP1L1(const RsForEachStubParamStruct *p,
+    static void kernelP1L1(const RsExpandKernelParams *p,
                            uint32_t xstart, uint32_t xend,
                            uint32_t instep, uint32_t outstep);
 
@@ -160,7 +160,7 @@ void RsdCpuScriptIntrinsicHistogram::postLaunch(uint32_t slot, const Allocation 
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -177,7 +177,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U4(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -193,7 +193,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U3(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -208,7 +208,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1U2(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -226,7 +226,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L4(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -243,7 +243,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L3(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -259,7 +259,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L2(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 
@@ -274,7 +274,7 @@ void RsdCpuScriptIntrinsicHistogram::kernelP1L1(const RsForEachStubParamStruct *
     }
 }
 
-void RsdCpuScriptIntrinsicHistogram::kernelP1U1(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicHistogram::kernelP1U1(const RsExpandKernelParams *p,
                                                 uint32_t xstart, uint32_t xend,
                                                 uint32_t instep, uint32_t outstep) {
 

cpu_ref/rsCpuIntrinsicYuvToRGB.cpp
@@ -46,7 +46,7 @@ public:
 protected:
     ObjectBaseRef<Allocation> alloc;
 
-    static void kernel(const RsForEachStubParamStruct *p,
+    static void kernel(const RsExpandKernelParams *p,
                        uint32_t xstart, uint32_t xend,
                        uint32_t instep, uint32_t outstep);
 };
@@ -101,7 +101,7 @@ extern "C" void rsdIntrinsicYuv_K(void *dst, const uchar *Y, const uchar *uv, ui
 extern "C" void rsdIntrinsicYuvR_K(void *dst, const uchar *Y, const uchar *uv, uint32_t xstart, size_t xend);
 extern "C" void rsdIntrinsicYuv2_K(void *dst, const uchar *Y, const uchar *u, const uchar *v, size_t xstart, size_t xend);
 
-void RsdCpuScriptIntrinsicYuvToRGB::kernel(const RsForEachStubParamStruct *p,
+void RsdCpuScriptIntrinsicYuvToRGB::kernel(const RsExpandKernelParams *p,
                                            uint32_t xstart, uint32_t xend,
                                            uint32_t instep, uint32_t outstep) {
     RsdCpuScriptIntrinsicYuvToRGB *cp = (RsdCpuScriptIntrinsicYuvToRGB *)p->usr;

cpu_ref/rsCpuScriptGroup.cpp
@@ -44,66 +44,71 @@ void CpuScriptGroupImpl::setOutput(const ScriptKernelID *kid, Allocation *a) {
 }
 
 
-typedef void (*ScriptGroupRootFunc_t)(const RsForEachStubParamStruct *p,
+typedef void (*ScriptGroupRootFunc_t)(const RsExpandKernelParams *kparams,
                                       uint32_t xstart, uint32_t xend,
                                       uint32_t instep, uint32_t outstep);
 
-void CpuScriptGroupImpl::scriptGroupRoot(const RsForEachStubParamStruct *p,
+void CpuScriptGroupImpl::scriptGroupRoot(const RsExpandKernelParams *kparams,
                                          uint32_t xstart, uint32_t xend,
                                          uint32_t instep, uint32_t outstep) {
 
 
-    const ScriptList *sl = (const ScriptList *)p->usr;
-    RsForEachStubParamStruct *mp = (RsForEachStubParamStruct *)p;
-    const void *oldUsr = p->usr;
+    const ScriptList *sl            = (const ScriptList *)kparams->usr;
+    RsExpandKernelParams *mkparams = (RsExpandKernelParams *)kparams;
 
-    for(size_t ct=0; ct < sl->count; ct++) {
+    for (size_t ct = 0; ct < sl->count; ct++) {
         ScriptGroupRootFunc_t func;
-        func = (ScriptGroupRootFunc_t)sl->fnPtrs[ct];
-        mp->usr = sl->usrPtrs[ct];
+        func          = (ScriptGroupRootFunc_t)sl->fnPtrs[ct];
+        mkparams->usr = sl->usrPtrs[ct];
 
-        mp->ptrIn = NULL;
-        mp->in = NULL;
-        mp->ptrOut = NULL;
-        mp->out = NULL;
+        mkparams->in  = NULL;
+        mkparams->out = NULL;
 
         uint32_t istep = 0;
         uint32_t ostep = 0;
 
         if (sl->ins[ct]) {
-            mp->ptrIn = (const uint8_t *)sl->ins[ct]->mHal.drvState.lod[0].mallocPtr;
+            mkparams->in =
+              (const uint8_t *)sl->ins[ct]->mHal.drvState.lod[0].mallocPtr;
+
             istep = sl->ins[ct]->mHal.state.elementSizeBytes;
-            mp->in = mp->ptrIn;
+
             if (sl->inExts[ct]) {
-                mp->in = mp->ptrIn + sl->ins[ct]->mHal.drvState.lod[0].stride * p->y;
-            } else {
-                if (sl->ins[ct]->mHal.drvState.lod[0].dimY > p->lid) {
-                    mp->in = mp->ptrIn + sl->ins[ct]->mHal.drvState.lod[0].stride * p->lid;
-                }
+                mkparams->in =
+                  (const uint8_t *)mkparams->in +
+                  sl->ins[ct]->mHal.drvState.lod[0].stride * kparams->y;
+
+            } else if (sl->ins[ct]->mHal.drvState.lod[0].dimY > kparams->lid) {
+                mkparams->in =
+                  (const uint8_t *)mkparams->in +
+                  sl->ins[ct]->mHal.drvState.lod[0].stride * kparams->lid;
             }
         }
 
         if (sl->outs[ct]) {
-            mp->ptrOut = (uint8_t *)sl->outs[ct]->mHal.drvState.lod[0].mallocPtr;
-            mp->out = mp->ptrOut;
+            mkparams->out =
+              (uint8_t *)sl->outs[ct]->mHal.drvState.lod[0].mallocPtr;
+
             ostep = sl->outs[ct]->mHal.state.elementSizeBytes;
+
             if (sl->outExts[ct]) {
-                mp->out = mp->ptrOut + sl->outs[ct]->mHal.drvState.lod[0].stride * p->y;
-            } else {
-                if (sl->outs[ct]->mHal.drvState.lod[0].dimY > p->lid) {
-                    mp->out = mp->ptrOut + sl->outs[ct]->mHal.drvState.lod[0].stride * p->lid;
-                }
+                mkparams->out =
+                  (uint8_t *)mkparams->out +
+                  sl->outs[ct]->mHal.drvState.lod[0].stride * kparams->y;
+
+            } else if (sl->outs[ct]->mHal.drvState.lod[0].dimY > kparams->lid) {
+                mkparams->out =
+                  (uint8_t *)mkparams->out +
+                  sl->outs[ct]->mHal.drvState.lod[0].stride * kparams->lid;
             }
         }
 
         //ALOGE("kernel %i %p,%p  %p,%p", ct, mp->ptrIn, mp->in, mp->ptrOut, mp->out);
-        func(p, xstart, xend, istep, ostep);
+        func(kparams, xstart, xend, istep, ostep);
     }
     //ALOGE("script group root");
 
-    //ConvolveParams *cp = (ConvolveParams *)p->usr;
-
-    mp->usr = oldUsr;
+    mkparams->usr = sl;
 }
 
 
@@ -245,5 +250,3 @@ void CpuScriptGroupImpl::execute() {
         }
     }
 }
-
-