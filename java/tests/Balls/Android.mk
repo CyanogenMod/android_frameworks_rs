9e8587a917a8783a817a32e54a63f311851dd6e4
Stephen Hines <srhines@google.com>
2014-08-15 02:14:54 -0700

    Fix RS tests to prepare for 64-bit compilation.
    Bug: 16031597
    Change-Id: Id21b1ade6e097804f6c4f9caf765bf2f6ba8037e

--- java/tests/Balls/Android.mk
+++ java/tests/Balls/Android.mk
@@ -22,5 +22,6 @@ LOCAL_MODULE_TAGS := tests
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
 
 LOCAL_PACKAGE_NAME := RsBalls
+LOCAL_SDK_VERSION := 14
 
 include $(BUILD_PACKAGE)
