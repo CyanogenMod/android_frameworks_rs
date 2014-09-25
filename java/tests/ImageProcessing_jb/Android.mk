721a5a24e2278f4012109865d62b98190b3185af
Jason Sams <jsams@google.com>
2014-09-25 15:47:12 -0700

    Overhaul the ImageProcessing_jb UI
    This is a step torward making the application good demo code
    for developers.  Some cleanup is still needed.  At this point
    it should be functional.
    
    bug 17939419
    
    
    Change-Id: I024531f71123fe66348d679d00e955cb9bc4106c

--- java/tests/ImageProcessing_jb/Android.mk
+++ java/tests/ImageProcessing_jb/Android.mk
@@ -17,6 +17,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_JAVA_LIBRARIES := android.test.runner
+
 LOCAL_MODULE_TAGS := tests
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
