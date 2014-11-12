3b1e2ee102c2b5e76430ac7e6cc38fdbf59d3a73
Dan Albert <danalbert@google.com>
2014-11-12 11:30:35 -0800

    Move libRScpp.so to libc++.
    Bug: 15193147
    Bug: 18341995
    Change-Id: I257e11cc76a1a15c1962f30315f20a6c5b1d416b

--- cpp/Android.mk
+++ cpp/Android.mk
@@ -38,7 +38,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += frameworks/rs
 LOCAL_C_INCLUDES += $(intermediates)
 
-include external/stlport/libstlport.mk
 include $(BUILD_SHARED_LIBRARY)
 
 
