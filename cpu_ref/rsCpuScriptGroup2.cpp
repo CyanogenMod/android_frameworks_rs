Schischu/aosp_android_framework_rs

Move use of tempnam to mkstemp.
Bug: 19340053
Change-Id: I1734e085beef395ebcc6453af1efaa19f7409e22

cpu_ref/rsCpuScriptGroup2.cpp
@@ -1,6 +1,9 @@
 #include "rsCpuScriptGroup2.h"
 
 #include <dlfcn.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <string>
 #include <vector>
@@ -288,9 +291,25 @@ void Batch::tryToCreateFusedKernel(const char *cacheDir) {
         slots.push_back(kernelID->mSlot);
     }
 
-    string outputPath(tempnam(cacheDir, "fused"));
-    string outputFileName = getFileName(outputPath);
-    string objFilePath(outputPath);
+    char tempPath[PATH_MAX];
+    if (cacheDir != NULL) {
+      size_t len = strlen(cacheDir);
+      if (len == 0 || cacheDir[len - 1] == '/') {
+        snprintf(tempPath, sizeof(tempPath), "%sfusedXXXXXX", cacheDir);
+      } else {
+        snprintf(tempPath, sizeof(tempPath), "%s/fusedXXXXXX", cacheDir);
+      }
+    } else {
+      snprintf(tempPath, sizeof(tempPath), "fusedXXXXXX");
+    }
+    int tempfd = mkstemp(tempPath);
+    if (tempfd == -1) {
+      return;
+    }
+    TEMP_FAILURE_RETRY(close(tempfd));
+
+    string outputFileName = getFileName(tempPath);
+    string objFilePath(tempPath);
     objFilePath.append(".o");
     string rsLibPath(SYSLIBPATH"/libclcore.bc");
     vector<const char*> arguments;
@@ -300,7 +319,7 @@ void Batch::tryToCreateFusedKernel(const char *cacheDir) {
             convertListToString(arguments.size() - 1, arguments.data());
 
     if (!fuseAndCompile(arguments.data(), commandLine)) {
-        return;
+        goto cleanup_tempfile;
     }
 
     //===--------------------------------------------------------------------===//
@@ -311,19 +330,20 @@ void Batch::tryToCreateFusedKernel(const char *cacheDir) {
 
     if (!SharedLibraryUtils::createSharedLibrary(cacheDir, resName)) {
         ALOGE("Failed to link object file '%s'", resName);
-        return;
+        goto cleanup_tempfile;
     }
 
     void* mSharedObj = SharedLibraryUtils::loadSharedLibrary(cacheDir, resName);
     if (mSharedObj == nullptr) {
         ALOGE("Unable to load '%s'", resName);
-        return;
+        goto cleanup_tempfile;
     }
 
     mExecutable = ScriptExecutable::createFromSharedObject(
                                                            nullptr,  // RS context. Unused.
                                                            mSharedObj);
-
+cleanup_tempfile:
+    unlink(tempPath);
 #endif  // RS_COMPATIBILITY_LIB
 }
 

CyanogenMod/android_frameworks_rs

cpu_ref/rsCpuScriptGroup2.cpp
@@ -1,6 +1,9 @@
 #include "rsCpuScriptGroup2.h"
 
 #include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include <string>
 #include <vector>
@@ -288,10 +291,17 @@ void Batch::tryToCreateFusedKernel(const char *cacheDir) {
         slots.push_back(kernelID->mSlot);
     }
 
-    string outputPath(tempnam(cacheDir, "fused"));
-    string outputFileName = getFileName(outputPath);
-    string objFilePath(outputPath);
-    objFilePath.append(".o");
+    rsAssert(cacheDir != nullptr);
+    string objFilePath(cacheDir);
+    objFilePath.append("/fusedXXXXXX.o");
+    // Find unique object file name, to make following file names unique.
+    int tempfd = mkstemps(&objFilePath[0], 2);
+    if (tempfd == -1) {
+      return;
+    }
+    TEMP_FAILURE_RETRY(close(tempfd));
+
+    string outputFileName = getFileName(objFilePath.substr(0, objFilePath.size() - 2));
     string rsLibPath(SYSLIBPATH"/libclcore.bc");
     vector<const char*> arguments;
     setupCompileArguments(inputFiles, slots, cacheDir, outputFileName, rsLibPath,
@@ -300,6 +310,7 @@ void Batch::tryToCreateFusedKernel(const char *cacheDir) {
             convertListToString(arguments.size() - 1, arguments.data());
 
     if (!fuseAndCompile(arguments.data(), commandLine)) {
+        unlink(objFilePath.c_str());
         return;
     }
 