Schischu/aosp_android_framework_rs

Added tests for ScriptGroup2 API to RsTest
Change-Id: Ib04499904b741b8054841b693e1c02c02a558cde

java/tests/RsTest/src/com/android/rs/test/RSTestCore.java
@@ -84,6 +84,8 @@ public class RSTestCore {
         unitTests.add(new UT_foreach(this, mRes, mCtx));
         unitTests.add(new UT_foreach_bounds(this, mRes, mCtx));
         unitTests.add(new UT_noroot(this, mRes, mCtx));
+        unitTests.add(new UT_sg2_pointwise(this, mRes, mCtx));
+        unitTests.add(new UT_sg2_gatherscatter(this, mRes, mCtx));
         unitTests.add(new UT_atomic(this, mRes, mCtx));
         unitTests.add(new UT_struct(this, mRes, mCtx));
         unitTests.add(new UT_math(this, mRes, mCtx));

CyanogenMod/android_frameworks_rs

java/tests/RsTest/src/com/android/rs/test/RSTestCore.java
@@ -85,6 +85,8 @@ public class RSTestCore {
         unitTests.add(new UT_foreach(this, mRes, mCtx));
         unitTests.add(new UT_foreach_bounds(this, mRes, mCtx));
         unitTests.add(new UT_noroot(this, mRes, mCtx));
+        unitTests.add(new UT_script_group2_pointwise(this, mRes, mCtx));
+        unitTests.add(new UT_script_group2_gatherscatter(this, mRes, mCtx));
         unitTests.add(new UT_atomic(this, mRes, mCtx));
         unitTests.add(new UT_struct(this, mRes, mCtx));
         unitTests.add(new UT_math(this, mRes, mCtx));