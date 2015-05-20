Schischu/aosp_android_framework_rs

Added in wait for attach context flag.
Added in a small flag which is  passed to the context from the Java/C++ API
which causes the context to delay execution until a debugger is a attached.
This gives time to the developer to attach a debugger to the process before
the kernel is executed.

Change-Id: I5bb8e75100436920f9e1a0f414f3b0bcf0d2b439
Signed-off-by: Stephen McGroarty <stephen@codeplay.com>

cpp/RenderScript.cpp
@@ -165,7 +165,7 @@ bool RS::init(const char * name, int targetApi, uint32_t flags) {
     }
 
     if (flags & ~(RS_CONTEXT_SYNCHRONOUS | RS_CONTEXT_LOW_LATENCY |
-                  RS_CONTEXT_LOW_POWER)) {
+                  RS_CONTEXT_LOW_POWER | RS_WAIT_FOR_ATTACH)) {
         ALOGE("Invalid flags passed");
         return false;
     }

CyanogenMod/android_frameworks_rs

(cherry picked from commit d5164d537c84867880694327727b5f22b0a3ccbd)

cpp/RenderScript.cpp
@@ -165,7 +165,7 @@ bool RS::init(const char * name, int targetApi, uint32_t flags) {
     }
 
     if (flags & ~(RS_CONTEXT_SYNCHRONOUS | RS_CONTEXT_LOW_LATENCY |
-                  RS_CONTEXT_LOW_POWER)) {
+                  RS_CONTEXT_LOW_POWER | RS_CONTEXT_WAIT_FOR_ATTACH)) {
         ALOGE("Invalid flags passed");
         return false;
     }