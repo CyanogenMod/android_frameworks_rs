Schischu/aosp_android_framework_rs

Add AllocationCreateStrided to support lib CPU driver
 - Able to create Allocations with arbitrary alignment requirement,
   making Incremental Intrinsic Support able to run with different
   native GPU RS runtime.
 - Make compat mode CPU driver using an additional rs_compat.spec.
 - Add a compat mode only USAGE_INCREMENTAL_SUPPORT.
 - Add AllocationCreateStrided() to take an Alignment requirement (power
   of 2). Only enbled when detect USAGE_INCREMENTAL_SUPPORT.

Change-Id: I66f913c3a2474f93af5a244c0c84460a7a395e71

driver/rsdAllocation.h
@@ -84,6 +84,11 @@ uint32_t rsdAllocationGrallocBits(const android::renderscript::Context *rsc,
 bool rsdAllocationInit(const android::renderscript::Context *rsc,
                        android::renderscript::Allocation *alloc,
                        bool forceZero);
+#ifdef RS_COMPATIBILITY_LIB
+bool rsdAllocationInitStrided(const android::renderscript::Context *rsc,
+                              android::renderscript::Allocation *alloc,
+                              bool forceZero, size_t bytesAligned);
+#endif
 bool rsdAllocationAdapterInit(const android::renderscript::Context *rsc,
                               android::renderscript::Allocation *alloc);
 void rsdAllocationDestroy(const android::renderscript::Context *rsc,

rsAllocation.cpp
@@ -59,8 +59,9 @@ void Allocation::operator delete(void* ptr) {
     }
 }
 
-Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32_t usages,
-                              RsAllocationMipmapControl mc, void * ptr) {
+Allocation * Allocation::createAllocationStrided(Context *rsc, const Type *type, uint32_t usages,
+                                                 RsAllocationMipmapControl mc, void * ptr,
+                                                 size_t byteAligned) {
     // Allocation objects must use allocator specified by the driver
     void* allocMem = rsc->mHal.funcs.allocRuntimeMem(sizeof(Allocation), 0);
 
@@ -79,6 +80,11 @@ Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32
             rsc->setError(RS_ERROR_FATAL_DRIVER, "Allocation Init called with USAGE_OEM but driver does not support it");
             return nullptr;
         }
+#ifdef RS_COMPATIBILITY_LIB
+    } else if (usages & RS_ALLOCATION_USAGE_INCREMENTAL_SUPPORT){
+        a = new (allocMem) Allocation(rsc, type, usages, mc, ptr);
+        success = rsc->mHal.funcs.allocation.initStrided(rsc, a, type->getElement()->getHasReferences(), byteAligned);
+#endif
     } else {
         a = new (allocMem) Allocation(rsc, type, usages, mc, ptr);
         success = rsc->mHal.funcs.allocation.init(rsc, a, type->getElement()->getHasReferences());
@@ -91,6 +97,11 @@ Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32
     }
 
     return a;
+}
+
+Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32_t usages,
+                              RsAllocationMipmapControl mc, void * ptr) {
+    return Allocation::createAllocationStrided(rsc, type, usages, mc, ptr, 16);
 }
 
 Allocation * Allocation::createAdapter(Context *rsc, const Allocation *alloc, const Type *type) {
@@ -719,6 +730,19 @@ RsAllocation rsi_AllocationCreateTyped(Context *rsc, RsType vtype,
                                        RsAllocationMipmapControl mipmaps,
                                        uint32_t usages, uintptr_t ptr) {
     Allocation * alloc = Allocation::createAllocation(rsc, static_cast<Type *>(vtype), usages, mipmaps, (void*)ptr);
+    if (!alloc) {
+        return nullptr;
+    }
+    alloc->incUserRef();
+    return alloc;
+}
+
+RsAllocation rsi_AllocationCreateStrided(Context *rsc, RsType vtype,
+                                         RsAllocationMipmapControl mipmaps,
+                                         uint32_t usages, uintptr_t ptr,
+                                         size_t byteAligned) {
+    Allocation * alloc = Allocation::createAllocationStrided(rsc, static_cast<Type *>(vtype), usages, mipmaps,
+                                                             (void*)ptr, byteAligned);
     if (!alloc) {
         return nullptr;
     }

rs_compat.spec
@@ -1,0 +1,9 @@
+AllocationCreateStrided {
+    direct
+    param RsType vtype
+    param RsAllocationMipmapControl mipmaps
+    param uint32_t usages
+    param uintptr_t ptr
+    param size_t byteAligned
+    ret RsAllocation
+}

rs_hal.h
@@ -279,6 +279,9 @@ typedef struct {
         void (*getPointer)(const Context *rsc, const Allocation *alloc,
                            uint32_t lod, RsAllocationCubemapFace face,
                            uint32_t z, uint32_t array);
+#ifdef RS_COMPATIBILITY_LIB
+        bool (*initStrided)(const Context *rsc, Allocation *alloc, bool forceZero, size_t byteAligned);
+#endif
     } allocation;
 
     struct {
@@ -407,6 +410,9 @@ enum RsHalInitEnums {
     RS_HAL_ALLOCATION_ADAPTER_OFFSET                        = 2025,
     RS_HAL_ALLOCATION_INIT_OEM                              = 2026,
     RS_HAL_ALLOCATION_GET_POINTER                           = 2027,
+#ifdef RS_COMPATIBILITY_LIB
+    RS_HAL_ALLOCATION_INIT_STRIDED                          = 2999,
+#endif
 
     RS_HAL_SAMPLER_INIT                                     = 3000,
     RS_HAL_SAMPLER_DESTROY                                  = 3001,

CyanogenMod/android_frameworks_rs

driver/rsdAllocation.h
@@ -84,6 +84,11 @@ uint32_t rsdAllocationGrallocBits(const android::renderscript::Context *rsc,
 bool rsdAllocationInit(const android::renderscript::Context *rsc,
                        android::renderscript::Allocation *alloc,
                        bool forceZero);
+#ifdef RS_COMPATIBILITY_LIB
+bool rsdAllocationInitStrided(const android::renderscript::Context *rsc,
+                              android::renderscript::Allocation *alloc,
+                              bool forceZero, size_t requiredAlignment);
+#endif
 bool rsdAllocationAdapterInit(const android::renderscript::Context *rsc,
                               android::renderscript::Allocation *alloc);
 void rsdAllocationDestroy(const android::renderscript::Context *rsc,

rsAllocation.cpp
@@ -59,8 +59,9 @@ void Allocation::operator delete(void* ptr) {
     }
 }
 
-Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32_t usages,
-                              RsAllocationMipmapControl mc, void * ptr) {
+Allocation * Allocation::createAllocationStrided(Context *rsc, const Type *type, uint32_t usages,
+                                                 RsAllocationMipmapControl mc, void * ptr,
+                                                 size_t requiredAlignment) {
     // Allocation objects must use allocator specified by the driver
     void* allocMem = rsc->mHal.funcs.allocRuntimeMem(sizeof(Allocation), 0);
 
@@ -79,6 +80,11 @@ Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32
             rsc->setError(RS_ERROR_FATAL_DRIVER, "Allocation Init called with USAGE_OEM but driver does not support it");
             return nullptr;
         }
+#ifdef RS_COMPATIBILITY_LIB
+    } else if (usages & RS_ALLOCATION_USAGE_INCREMENTAL_SUPPORT){
+        a = new (allocMem) Allocation(rsc, type, usages, mc, ptr);
+        success = rsc->mHal.funcs.allocation.initStrided(rsc, a, type->getElement()->getHasReferences(), requiredAlignment);
+#endif
     } else {
         a = new (allocMem) Allocation(rsc, type, usages, mc, ptr);
         success = rsc->mHal.funcs.allocation.init(rsc, a, type->getElement()->getHasReferences());
@@ -91,6 +97,11 @@ Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32
     }
 
     return a;
+}
+
+Allocation * Allocation::createAllocation(Context *rsc, const Type *type, uint32_t usages,
+                              RsAllocationMipmapControl mc, void * ptr) {
+    return Allocation::createAllocationStrided(rsc, type, usages, mc, ptr, kMinimumRSAlignment);
 }
 
 Allocation * Allocation::createAdapter(Context *rsc, const Allocation *alloc, const Type *type) {
@@ -719,6 +730,19 @@ RsAllocation rsi_AllocationCreateTyped(Context *rsc, RsType vtype,
                                        RsAllocationMipmapControl mipmaps,
                                        uint32_t usages, uintptr_t ptr) {
     Allocation * alloc = Allocation::createAllocation(rsc, static_cast<Type *>(vtype), usages, mipmaps, (void*)ptr);
+    if (!alloc) {
+        return nullptr;
+    }
+    alloc->incUserRef();
+    return alloc;
+}
+
+RsAllocation rsi_AllocationCreateStrided(Context *rsc, RsType vtype,
+                                         RsAllocationMipmapControl mipmaps,
+                                         uint32_t usages, uintptr_t ptr,
+                                         size_t requiredAlignment) {
+    Allocation * alloc = Allocation::createAllocationStrided(rsc, static_cast<Type *>(vtype), usages, mipmaps,
+                                                             (void*)ptr, requiredAlignment);
     if (!alloc) {
         return nullptr;
     }

rs_compat.spec
@@ -1,0 +1,9 @@
+AllocationCreateStrided {
+    direct
+    param RsType vtype
+    param RsAllocationMipmapControl mipmaps
+    param uint32_t usages
+    param uintptr_t ptr
+    param size_t requiredAlignment
+    ret RsAllocation
+}

rs_hal.h
@@ -279,6 +279,9 @@ typedef struct {
         void (*getPointer)(const Context *rsc, const Allocation *alloc,
                            uint32_t lod, RsAllocationCubemapFace face,
                            uint32_t z, uint32_t array);
+#ifdef RS_COMPATIBILITY_LIB
+        bool (*initStrided)(const Context *rsc, Allocation *alloc, bool forceZero, size_t requiredAlignment);
+#endif
     } allocation;
 
     struct {
@@ -407,6 +410,9 @@ enum RsHalInitEnums {
     RS_HAL_ALLOCATION_ADAPTER_OFFSET                        = 2025,
     RS_HAL_ALLOCATION_INIT_OEM                              = 2026,
     RS_HAL_ALLOCATION_GET_POINTER                           = 2027,
+#ifdef RS_COMPATIBILITY_LIB
+    RS_HAL_ALLOCATION_INIT_STRIDED                          = 2999,
+#endif
 
     RS_HAL_SAMPLER_INIT                                     = 3000,
     RS_HAL_SAMPLER_DESTROY                                  = 3001,